6a1637d46dc9ea5e60a4758101b4d0d99fd7b4c9
jeeonglee <jeeonglee@gmail.com>
2020-11-17 21:04:30 -0300

    rotas pt1

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require("express")
 
 let app = express()
+
+
+app.get("/contatos", (req, res) => res.send("Pagina de Contatos"))
+//criando um servidor
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
